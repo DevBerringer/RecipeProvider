cd08fa2ccffb6ff196767f327b6f368dace6dce8
devBerringer <blakeberringer@gmail.com>
2025-07-06 16:38:23 -0500

    first prod push

--- src/main/java/com/mbapps/fc/provider/FamilyCookbookApplication.java
+++ src/main/java/com/mbapps/fc/provider/FamilyCookbookApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FamilyCookbookApplication {
     public static void main(String[] args) {
-        System.getenv().forEach((k, v) -> System.out.println(k + "=" + v));
         SpringApplication.run(FamilyCookbookApplication.class, args);
     }
 }
