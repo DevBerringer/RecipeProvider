206c12109e29f1e3a61bb378dd21c0779df92913
devBerringer <blakeberringer@gmail.com>
2025-07-06 16:22:48 -0500

    first prod push

--- src/main/java/com/mbapps/fc/provider/FamilyCookbookApplication.java
+++ src/main/java/com/mbapps/fc/provider/FamilyCookbookApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FamilyCookbookApplication {
     public static void main(String[] args) {
-        System.out.println("APP_COOKIE_SECURE: " + System.getenv("APP_COOKIE_SECURE"));
         SpringApplication.run(FamilyCookbookApplication.class, args);
     }
 }
